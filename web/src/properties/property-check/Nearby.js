12b858700666e28ef804affc69cd071e44ed2000
Arend Oppewal <ao@crackedjar.com>
2020-06-21 18:42:12 +1000

    Embed map

---
+++ web/src/properties/property-check/Nearby.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Nearby = () => {
+  return (
+    <div className="py-2">
+      <h3 className="font-bold text-md">Nearby</h3>
+      <div>
+        <h4 className="font-bold">Stations</h4>
+      </div>
+      <div>
+        <h4 className="font-bold">???</h4>
+      </div>
+    </div>
+  );
+};
+
+export default Nearby;
--- web/src/properties/property-check/PropertyCheck.js
+++ web/src/properties/property-check/PropertyCheck.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Address from '../common/Address';
 import NbnDetails from './NbnDetails';
+import Nearby from './Nearby';
 
 const PropertyCheck = () => {
   const [address, setAddress] = useState({
@@ -19,14 +20,30 @@ const PropertyCheck = () => {
   const mapQuery = encodeURIComponent(`${address.street} ${address.suburb}`);
   const mapLink = `https://www.google.com/maps/search/
   ?api=1&query=${mapQuery}&query_place_id=${placeId}`;
+  const embeddedMap = !placeId
+    ? ''
+    : // eslint-disable-next-line max-len
+      `https://www.google.com/maps/embed/v1/place?q=place_id:${placeId}&key=${process.env.REACT_APP_GOOGLE_APIKEY}`;
 
   return (
     <section className="w-full">
       <h2 className="font-bold text-lg">Property Check</h2>
       <Address address={address} changeHandler={onAutoComplete} />
-      <div hidden={!placeId} className="cursor-pointer">
-        <a href={mapLink}>Google Maps</a>
+      <div hidden={!placeId}>
+        <iframe
+          title="Property Map"
+          width="100%"
+          height="450"
+          frameBorder="0"
+          style={{ border: 0 }}
+          src={embeddedMap}
+          allowFullScreen
+        />
+        <a href={mapLink} className="cursor-pointer block text-purple-400 hover:text-purple-700">
+          View in Google Maps
+        </a>
       </div>
+      <Nearby />
       <NbnDetails address={address} />
     </section>
   );
