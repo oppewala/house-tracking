ea7d139dda7dc81a0785bf7527ff1edcd5435f1e
Arend Oppewal <ao@crackedjar.com>
2020-10-05 22:29:43 +1100

    Removed unused import

--- web/src/feature/Properties/property-directory/Property.tsx
+++ web/src/feature/Properties/property-directory/Property.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent } from 'react';
-import { Card, Link, Typography } from '@material-ui/core';
+import { Card, Typography } from '@material-ui/core';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import Button from '@material-ui/core/Button';
