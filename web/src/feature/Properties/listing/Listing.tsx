e6c3c5271b7f7c7c8fc2f3fcf9822bae9430f60a
Arend Oppewal <ao@crackedjar.com>
2020-11-23 17:18:26 +1100

    Formatted listing details

--- web/src/feature/Properties/listing/Listing.tsx
+++ web/src/feature/Properties/listing/Listing.tsx
@@ -3,6 +3,9 @@ import { RetrieveProperty } from '../../../_services/ApiService/houseApi';
 import { Typography } from '@material-ui/core';
 import { BreadcrumbNav } from '../components/Breadcrumb/BreadcrumbNav';
 import { Property } from '../../../_services/ApiService/types';
+import KingBedOutlinedIcon from '@material-ui/icons/KingBedOutlined';
+import BathtubOutlinedIcon from '@material-ui/icons/BathtubOutlined';
+import DriveEtaOutlinedIcon from '@material-ui/icons/DriveEtaOutlined';
 
 interface Props {
   id: string;
@@ -39,12 +42,31 @@ export const Listing: FunctionComponent<Props> = ({ id }) => {
   };
   const address = buildAddress(property);
 
+  const HouseLayout: React.FC<{ house: Property | undefined }> = ({ house }) => {
+    return house === undefined ? null : (
+      <p>
+        <KingBedOutlinedIcon fontSize={'inherit'} /> Bedrooms: {house?.Layout?.Bedrooms} |{' '}
+        <BathtubOutlinedIcon fontSize={'inherit'} /> Bathrooms: {house?.Layout?.Bathrooms} |{' '}
+        <DriveEtaOutlinedIcon fontSize={'inherit'} /> Parking: {house?.Layout?.Parking}
+      </p>
+    );
+  };
+
   return (
     <>
       <BreadcrumbNav crumbs={[{ name: 'Properties', route: '/Properties' }]}>
         {address}
       </BreadcrumbNav>
       <Typography variant={'h4'}>{property?.Price}</Typography>
+      <HouseLayout house={property} />
+      <textarea>{property?.Notes}</textarea>
+      <ul>
+        {property?.References?.map((r) => (
+          <li key={r.Value}>
+            <a href={r.Value}>{r.Value}</a>
+          </li>
+        ))}
+      </ul>
       <div>
         {apiState === 'loading' ? (
           <Loading />
